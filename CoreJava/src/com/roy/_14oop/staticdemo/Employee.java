5e255aec12c40ee4f67b2d160dce1f6696a51ada
santyroy <sougata.roy26@yahoo.com>
2021-09-05 00:10:38 +0530

    lesson static

---
+++ CoreJava/src/com/roy/_14oop/staticdemo/Employee.java
@@ -0,0 +1,8 @@
+package com.roy._14oop.staticdemo;
+
+public class Employee extends User {
+
+    public static void doAnyStaticAction() {
+        System.out.println("Static method in Employee class is called.");
+    }
+}
