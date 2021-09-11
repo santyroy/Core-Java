5fd6793741d5036ce739aef880d8b1a9bb7ebe20
Sougata Roy <sougata.roy26@yahoo.com>
2021-09-12 01:07:44 +0530

    lesson stream

--- CoreJava/src/com/roy/test/ClassA.java
+++
@@ -1,8 +0,0 @@
-package com.roy.test;
-
-public class ClassA {
-
-    public void printA() {
-        System.out.println("This is Class A");
-    }
-}
--- CoreJava/src/com/roy/test/ClassB.java
+++
@@ -1,9 +0,0 @@
-package com.roy.test;
-
-public class ClassB extends ClassA {
-
-    @Override
-    public void printA() {
-        System.out.println("This is Class B");
-    }
-}
